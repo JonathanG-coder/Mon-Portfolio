c102bece0b17fc333a7ffb2d7581c8c21eae9507
jonathan <jonathangager75@gmail.com>
2024-10-12 09:37:22 +0200

    Création du lien URL sur github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,6 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
-// If you are not going to use vercel analytics you can delete the analytics
-// If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: "static",  // Génére un site statique
+  base: "/Mon-Portfolio/", // Remplace par le nom de ton dépôt GitHub
 });
-
-//If you don't have vercel the configuration will be empty
-
-//ej : export default defineConfig({});

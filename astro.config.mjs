2f7eed9e667f666485fd5c73283a9743581107ac
jonathan <jonathangager75@gmail.com>
2024-10-12 09:53:23 +0200

    Lien URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  output: "static",  // Génére un site statique
-  base: "/Mon-Portfolio/", // Remplace par le nom de ton dépôt GitHub
+  output: "static", 
+  base: "/Mon-Portfolio/", 
 });
